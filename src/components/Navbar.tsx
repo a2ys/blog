c2a46a1fbf8bf32be1008a6dc9c8761fce92aa0a
Aayush Shukla <oreus.aks@gmail.com>
2024-10-08 23:00:22 +0530

    feat: create a basic Navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <div className="relative py-6 px-12 font-extrabold text-2xl border-b-2 border-b-neutral-950 shadow-md">
+      <a href="/">Blog</a>
+    </div>
+  );
+};
+
+export default Navbar;
