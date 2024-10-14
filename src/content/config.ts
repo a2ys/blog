04c3a21d1c63ccae97b58399547b66c7cbaa09e2
Aayush Shukla <oreus.aks@gmail.com>
2024-10-14 18:58:00 +0530

    feat: add schema for author collection and update schema for blog collection

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,14 +1,26 @@
-import { z, defineCollection } from "astro:content";
+import { z, defineCollection, reference } from "astro:content";
 
 const blogCollection = defineCollection({
   type: "content",
   schema: z.object({
     title: z.string(),
     tags: z.array(z.string()).optional(),
+    // author: reference("author"),
+    author: z.string(),
     draft: z.boolean().default(false),
     date: z.date().transform((val) => new Date(val)),
     category: z.array(z.string()).optional(),
     description: z.string(),
+  }),
+});
+
+const authorCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    name: z.string(),
+    avatar: z.string(),
+    bio: z.string(),
+    talksAbout: z.array(z.string()),
   }),
 });
 
