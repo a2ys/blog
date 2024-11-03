5bf20b8270e966534e2a42f9d9218507ef5623a2
Aayush Shukla <oreus.aks@gmail.com>
2024-11-03 22:26:43 +0530

    feat: site theme revamp in progress

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,13 @@
 const Navbar = () => {
   return (
-    <div className="relative py-6 px-12 font-extrabold text-2xl border-b-2 border-b-neutral-950 shadow-md">
-      <a href="/">Blog</a>
+    <div className="relative py-4 px-8 sm:px-12 md:px-16 lg:py-4 lg:px-24 xl:py-6 xl:px-48 text-white text-lg sm:text-xl md:text-2xl navbar bg-gray-900 border-b border-b-white flex justify-between">
+      <div className="font-bold">
+        <a href="https://a2ys.dev" className="link">
+          a2ys
+        </a>{" "}
+        <span className="px-1 font-bold">/</span> <a href="/">Blog</a>
+      </div>
+      {/* Content will be added soon */}
     </div>
   );
 };
