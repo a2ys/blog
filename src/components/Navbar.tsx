7f270fba23f1752bff8cf58e274151f921d374b2
Aayush Shukla <oreus.aks@gmail.com>
2024-11-04 10:22:01 +0530

    feat: added links for posts and authors

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,20 @@
 const Navbar = () => {
   return (
     <div className="relative py-4 px-8 sm:px-12 md:px-16 lg:py-4 lg:px-24 xl:py-6 xl:px-48 text-white text-lg sm:text-xl md:text-2xl navbar bg-gray-900 border-b border-b-white flex justify-between">
-      <div className="font-bold">
+      <div className="blog-headers font-bold">
         <a href="https://a2ys.dev" className="link">
           a2ys
         </a>{" "}
         <span className="px-1 font-bold">/</span> <a href="/">Blog</a>
       </div>
-      {/* Content will be added soon */}
+      <div className="space-x-6">
+        <a href="/posts" className="link text-xl">
+          Posts
+        </a>
+        <a href="/authors" className="link text-xl">
+          Authors
+        </a>
+      </div>
     </div>
   );
 };
