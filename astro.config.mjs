6a445689c8e7586bf543f0889f7e0dd4f79c5e71
Aayush Shukla <oreus.aks@gmail.com>
2024-12-06 05:29:11 +0530

    feat: Add Math support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,27 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
-import vercel from '@astrojs/vercel/serverless';
-import { remarkReadingTime } from './remark-reading-time.mjs';
+import vercel from "@astrojs/vercel/serverless";
+import { remarkReadingTime } from "./remark-reading-time.mjs";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
 // https://astro.build/config
 export default defineConfig({
   markdown: {
     shikiConfig: {
-      theme: 'material-theme-ocean',
+      theme: "material-theme-ocean",
       defaultColor: false,
     },
-    remarkPlugins: [remarkReadingTime]
+    remarkPlugins: [remarkReadingTime, remarkMath],
+    rehypePlugins: [rehypeKatex],
   },
 
   integrations: [react(), tailwind()],
   output: "server",
-  adapter: vercel()
+  adapter: vercel(),
 });
